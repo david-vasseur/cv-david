d8c85b1091c60b9b71c204349d9cf348e140b40f
p5y4 <p5y4@laposte.net>
2024-01-09 19:16:10 +0100

    mise a jour

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -5,7 +5,7 @@ export const experienceData = [
         name: "Story",
         location: "Avignon",
         job: "Directeur Commercial",
-        logo: '${process.env.PUBLIC_URL}/media/story.jpg',
+        logo: `${process.env.PUBLIC_URL}/media/story.jpg`,
         positionTitle: "Dans le cadre de mes fonctions, je m'investis activement dans la prospection commerciale, identifiant de manière stratégique et développant un portefeuille client existant. De plus, je contribue à la croissance continue de l'entreprise en élaborant de nouveaux produits et en pilotant des projets innovants. Cela implique de rester informé des tendances de l'industrie, des demandes du marché et des avancées technologiques pour garantir que les offres restent compétitives et tournées vers l'avenir. Mes responsabilités s'étendent à la supervision complète du cycle de vie des opérations commerciales, assurant un processus fluide de l'initiation à la réalisation. Je prends en charge la gestion globale du magasin, y compris le contrôle des stocks et la supervision du personnel, garantissant une efficacité opérationnelle et la satisfaction des clients. Cette approche polyvalente me permet de contribuer de manière significative à la réussite globale et à la pérennité de l'entreprise.",
         date: "2021 - 2023",
         site: "https://www.story.fr"
@@ -16,7 +16,7 @@ export const experienceData = [
         name: "FJS",
         location: "Toute la France",
         job: "Commercial Senior Itinerant",
-        logo: '${process.env.PUBLIC_URL}/media/FJS-UNPLUGGED-Logo.png',
+        logo: `${process.env.PUBLIC_URL}/media/FJS-UNPLUGGED-Logo.png`,
         positionTitle: "En tant que commercial senior itinérant, je suis chargé de mener des missions à travers la France, que ce soit dans des magasins ou lors de foires. Mon rôle essentiel consiste à prospecter et à gérer un portefeuille client stratégique, en mettant en œuvre des stratégies adaptées à chaque contexte local. Je suis également impliqué dans le développement de nouveaux produits et de projets innovants, contribuant ainsi à la croissance constante de l'entreprise. Ma responsabilité s'étend à garantir le bon déroulement des opérations commerciales, de leur initiation à leur conclusion, tout en supervisant la gestion opérationnelle des magasins, des stocks et du personnel. Mon expérience et ma polyvalence sont des atouts essentiels dans la réussite de missions variées à travers le pays.",
         date: "2018 - 2021",
         site: "https://www.google.com"
@@ -27,7 +27,7 @@ export const experienceData = [
         name: "XXL",
         location: "Nimes",
         job: "Commercial Junior",
-        logo: '${process.env.PUBLIC_URL}/media/xxl.jpg',
+        logo: `${process.env.PUBLIC_URL}/media/xxl.jpg`,
         positionTitle: "En tant que commercial junior chez XXL, une entreprise spécialisée dans la vente de meubles haut de gamme, j'ai acquis une solide expérience dans le domaine de la vente et du service client. Ma mission consistait à conseiller les clients sur les produits haut de gamme, à répondre à leurs besoins spécifiques et à garantir une expérience d'achat exceptionnelle. J'ai travaillé en étroite collaboration avec des clients exigeants, développant ainsi mes compétences en écoute active et en communication. Mon rôle comprenait également la gestion des transactions, la présentation des produits de manière attrayante, et la création d'une atmosphère accueillante dans le magasin. Mon passage chez XXL m'a permis d'acquérir une connaissance approfondie du secteur des meubles haut de gamme et a été une étape cruciale dans mon apprentissage du métier de commercial.",
         date: "2015 - 2018",
         site: "https://www.xxl.fr"
@@ -38,7 +38,7 @@ export const experienceData = [
         name: "Litrimarché",
         location: "Ales",
         job: "Directeur de magasin",
-        logo: '${process.env.PUBLIC_URL}/media/logo-enseigne-literie-litrimarche-32.jpeg',
+        logo: `${process.env.PUBLIC_URL}/media/logo-enseigne-literie-litrimarche-32.jpeg`,
         positionTitle: "En tant que directeur de magasin chez Litrimarché, j'ai occupé un rôle clé dans la gestion complète des opérations quotidiennes du magasin. Mes responsabilités comprenaient la supervision du personnel, la gestion des stocks, et la garantie d'une expérience client optimale. J'ai dirigé des équipes pour atteindre les objectifs de vente, tout en veillant à maintenir des normes élevées de service client. Mon rôle a également impliqué la mise en œuvre de stratégies de marketing local pour stimuler les ventes et accroître la notoriété de la marque. Grâce à ma capacité à prendre des décisions éclairées et à motiver le personnel, j'ai contribué à la croissance constante du chiffre d'affaires du magasin et à la satisfaction de la clientèle. Mon expérience en tant que directeur de magasin a renforcé mes compétences en leadership et en gestion opérationnelle dans un environnement de vente au détail dynamique.",
         date: "2013 - 2015",
         site: "https://www.litrimarche.fr"
@@ -49,7 +49,7 @@ export const experienceData = [
         name: "Maison du monde",
         location: "Ales",
         job: "Vendeur - Magasinier",
-        logo: '${process.env.PUBLIC_URL}/media/mdm.png',
+        logo: `${process.env.PUBLIC_URL}/media/mdm.png`,
         positionTitle: "En tant que directeur de magasin chez Litrimarché, j'ai occupé un rôle clé dans la gestion complète des opérations quotidiennes du magasin. Mes responsabilités comprenaient la supervision du personnel, la gestion des stocks, et la garantie d'une expérience client optimale. J'ai dirigé des équipes pour atteindre les objectifs de vente, tout en veillant à maintenir des normes élevées de service client. Mon rôle a également impliqué la mise en œuvre de stratégies de marketing local pour stimuler les ventes et accroître la notoriété de la marque. Grâce à ma capacité à prendre des décisions éclairées et à motiver le personnel, j'ai contribué à la croissance constante du chiffre d'affaires du magasin et à la satisfaction de la clientèle. Mon expérience en tant que directeur de magasin a renforcé mes compétences en leadership et en gestion opérationnelle dans un environnement de vente au détail dynamique.",
         date: "2012 - 2013",
         site: "https://www.maisonsdumonde.com"
@@ -60,7 +60,7 @@ export const experienceData = [
         name: "KFC",
         location: "Ales",
         job: "Directeur de restaurant",
-        logo: '${process.env.PUBLIC_URL}/media/kfc.jpg',
+        logo: `${process.env.PUBLIC_URL}/media/kfc.jpg`,
         positionTitle: "En tant que directeur de magasin chez Litrimarché, j'ai occupé un rôle clé dans la gestion complète des opérations quotidiennes du magasin. Mes responsabilités comprenaient la supervision du personnel, la gestion des stocks, et la garantie d'une expérience client optimale. J'ai dirigé des équipes pour atteindre les objectifs de vente, tout en veillant à maintenir des normes élevées de service client. Mon rôle a également impliqué la mise en œuvre de stratégies de marketing local pour stimuler les ventes et accroître la notoriété de la marque. Grâce à ma capacité à prendre des décisions éclairées et à motiver le personnel, j'ai contribué à la croissance constante du chiffre d'affaires du magasin et à la satisfaction de la clientèle. Mon expérience en tant que directeur de magasin a renforcé mes compétences en leadership et en gestion opérationnelle dans un environnement de vente au détail dynamique.",
         date: "2007 - 2011",
         site: "https://www.kfc.fr"
